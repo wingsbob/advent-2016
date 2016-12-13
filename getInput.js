d9ddd920c39a083cff15b9373ff45f7c5017ccbb
Matthew Bull <matt_wings@hotmail.com>
2016-12-13 17:01:52 +0000

    Extracted cookie into configuration file

--- getInput.js
+++ getInput.js
@@ -1,9 +1,10 @@
 const rp = require('request-promise-native');
+const { Cookie } = require('./config.json');
 
-module.exports = (day) =>
+module.exports = day =>
   rp({
     uri: `http://adventofcode.com/2016/day/${day}/input`,
     headers: {
-      Cookie: '_gat=1; session=53616c7465645f5ff2cacca681dc59dbc5519bf7ba1e9ccf6ae7603b596632f02e006982379bda139b53231135b3c490; _ga=GA1.2.880196673.1480960251'
+      Cookie
     }
   });
